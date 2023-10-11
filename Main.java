43d0b4d2ba1eeacf4eff46482425aff181b317a9
satyam <satyamraghuvanshi60@gmail.com>
2023-10-11 12:37:06 +0530

    Update Main.java
    commit satyam

--- Main.java
+++ Main.java
@@ -5,5 +5,6 @@ public static void main(String[] args) {
 System.out.println("This is java program");
 System.out.println("output from java program");
 System.out.println(new Date());
+System.out.print("satyam");
 }
 }
